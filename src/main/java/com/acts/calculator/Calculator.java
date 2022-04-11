d255d230aa892482038ea82e644e3a5a2b6af59b
past18 <pastefinivw@gmail.com>
2022-04-11 16:13:41 +0300

    5. Arithmetic operations.

---
+++ src/main/java/com/acts/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.acts.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int ten = 10;
+        int sixty = 60;
+        int add = ten + sixty;
+        int sub = sixty - ten;
+        int multiply = sixty * ten;
+        int div = sixty / ten;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multiply);
+        System.out.println(div);
+    }
+}
