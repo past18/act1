3ea6beeec2d14c0911a57f851e5a90c8e38793ef
past18 <pastefinivw@gmail.com>
2022-04-11 16:01:11 +0300

    4. Variables.

---
+++ src/main/java/com/acts/Variables.java
@@ -0,0 +1,14 @@
+package com.acts;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte age = 22;
+        char sex = 'M';
+        short height = 186;
+        int fromearthtomoon = 384400;
+        boolean workPermit = true;
+        long l = 9223372036854775807L;
+        float f = 3.4e+38f;
+        double d = 1.7e+308;
+    }
+}
