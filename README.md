1849112bdcf3c6a48e5380cf1945362effb91db4
past18 <pastefinivw@gmail.com>
2022-04-11 12:47:42 +0300

    2. git upload first project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Act1. Block basic syntax.
+
+This project considered:
+- variables
+- branch operators
+- cycles
+- arrays
+- unit testing
+- IDEA
+- GIT basics
---
+++ src/test/java/com/acts/MainTest.java
@@ -0,0 +1,14 @@
+package com.acts;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainTest {
+    @Test
+    public void whenActionThenResult() {
+        int excepted = 1;
+        int result = 1;
+        assertThat(result, is(excepted));
+    }
+}
