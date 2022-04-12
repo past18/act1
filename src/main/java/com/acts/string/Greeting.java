db4a56ab11f1e709592fb39f6a517e0ddd8b3cc7
past18 <pastefinivw@gmail.com>
2022-04-12 12:32:50 +0300

    9. String.

---
+++ src/main/java/com/acts/string/Greeting.java
@@ -0,0 +1,12 @@
+package com.acts.string;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        idea = idea + "But im a newbie! ";
+        int year = 2022;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
